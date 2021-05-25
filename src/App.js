8961fda2e19fa3a7908e93799d14b586da00c597
Basir <basir.jafarzadeh@gmail.com>
2021-05-25 22:09:49 +0430

    Use Context API

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Counter from './Counter';
 import NewCounter from './NewCounter';
 import UserInfo from './UserInfo';
 import NewUserInfo from './NewUserInfo';
+import Layout from './Layout';
+import MainContent from './MainContent';
 
 function App() {
   return (
-    <div>
-      {/* <UserInfo /> */}
-      <NewUserInfo />
-    </div>
+    <Layout>
+      <MainContent />
+    </Layout>
   );
 }
 
